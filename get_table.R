9c950c843ca43bd6028ed34b9371f63d5ba4a15b
sidchop <sid.chopra@monash.edu>
2023-03-01 14:43:50 -0500

    kf fix

--- get_table.R
+++ get_table.R
@@ -1,4 +1,7 @@
 library(googlesheets4)
+googlesheets4::gs4_deauth()
+#googlesheets4::gs4_auth()
+
 #download table
 x <- read_sheet('1vTqeTSV1K4SfeH7KWtSDPHKJMhNsRcB3W3rYNRdxQKc', sheet = 1)
 saveRDS(x,"www/table.RDS")
